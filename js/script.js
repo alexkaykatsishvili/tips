5fd2b8e6a0f1b82a4d9665501049885830b1d109
Alexander Kaykatsishvili <alexander.kaykatsishvili@gmail.com>
2018-01-19 20:56:03 +0200

    first commit v.1.0

---
+++ js/script.js
@@ -0,0 +1,13 @@
+;(function($){
+	"use strict";
+
+	$('.slider').slick({
+      dots: true,
+      autoplay: true,
+      autoplaySpeed: 5000,
+      infinite: true,
+      fade: true,
+      cssEase: 'linear',
+      arrows: false
+  });
+})(jQuery);
